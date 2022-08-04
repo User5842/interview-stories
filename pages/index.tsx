54a875ebfad93772ec4f5a2034e2446231e30606
Rafael Negron <r.negron1@umiami.edu>
2022-08-03 23:25:25 -0400

    adding prettier and modifying HEAD;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <h1>Hello!</h1>;
+  return (
+    <div>
+      <Head>
+        <title>Interview Stories</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
 };
 
 export default Home;
