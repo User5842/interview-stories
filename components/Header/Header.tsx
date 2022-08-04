af52915a29f2c11e430b74da5d02f729496d93f1
Rafael Negron <r.negron1@umiami.edu>
2022-08-04 00:48:19 -0400

    beginning to create story create form;

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,39 @@
 import Link from "next/link";
+import { useRef } from "react";
+import StoryCreate from "../StoryCreate/StoryCreate";
 
 import styles from "./Header.module.css";
 
-const Header = () => (
-  <header className={styles.header}>
-    <h1 className={styles.header__heading}>Interview Stories</h1>
-    <nav className={styles.header__controls}>
-      <Link href="/">
-        <a className={styles.header__control}>View Stories</a>
-      </Link>
-      <button className={styles.header__control}>Create Story</button>
-    </nav>
-  </header>
-);
+const Header = () => {
+  /**
+   * Used to reference the `HTMLDialogElement`.
+   */
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+  /**
+   * Opens the create story dialog.
+   * @returns {void}
+   */
+  const onCreateStory = (): void => dialogRef.current?.showModal();
+
+  return (
+    <>
+      <dialog className={styles.dialog} ref={dialogRef}>
+        <StoryCreate />
+      </dialog>
+      <header className={styles.header}>
+        <h1 className={styles.header__heading}>Interview Stories</h1>
+        <nav className={styles.header__controls}>
+          <Link href="/">
+            <a className={styles.header__control}>View Stories</a>
+          </Link>
+          <button className={styles.header__control} onClick={onCreateStory}>
+            Create Story
+          </button>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Header;
---
+++ components/StoryCreate/StoryCreate.tsx
@@ -0,0 +1,25 @@
+import styles from "./StoryCreate.module.css";
+
+const StoryCreate = () => {
+  return (
+    <>
+      <h1 className={styles.dialog__heading}>What's your story?</h1>
+      <form>
+        <label htmlFor="company">
+          Company:
+          <input id="company" type="text" />
+        </label>
+        <label htmlFor="level">
+          Level:
+          <input id="level" type="text" />
+        </label>
+        <label htmlFor="offer">
+          Offer:
+          <input id="offer" type="checkbox" />
+        </label>
+      </form>
+    </>
+  );
+};
+
+export default StoryCreate;
